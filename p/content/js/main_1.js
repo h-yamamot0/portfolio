7383b7570dcc11f8469f46b3dcbcaff423b8328b
H.yamamoto <h.yamamoto.324@gmail.com>
2020-10-29 01:58:39 +0900

    loading

--- p/content/js/main_1.js
+++ p/content/js/main_1.js
@@ -1,13 +1,93 @@
 $(function(){
 
+// loading
+$(window).load(function () {
+	$('.js_load').delay(900).fadeOut(800);
+	// $('#loader').delay(600).fadeOut(300);
+	// $('#wrap').css('display', 'block');
+  })
+//10秒たったら強制的にロード画面を非表示
+$(function(){
+	setTimeout(stopload(),10000);
+});
+	
+function stopload(){
+	// $('#wrap').css('display','block');
+	$('.js_load').delay(900).fadeOut(800);
+	// $('#loader').delay(600).fadeOut(300);
+}
+
+// スムーススクロール
+$('a[href^="#"]').click(function () {
+	var adjust = 0;
+	var speed = 500;
+	var href = $(this).attr("href");
+	var target = $(href == "#" || href == "" ? 'html' : href);
+	var position = target.offset().top + adjust;
+	$("html, body").animate({ scrollTop: position }, speed, "swing");
+	return false;
+});
+
+
+// 現在位置表示
+var set = 200 //変化位置
+var boxTop = [];
+var current = -1;
+
+// コンテンツ測定
+function ctsHt() {
+	$('.main_cts').each(function(i) {
+		boxTop[i] = $(this).offset().top;
+	});
+}
+
+// navの表示
+function changeBox(secNum) {
+	if (secNum != current) {
+		current = secNum;
+		// secNum2 = secNum + 1;
+		$('.main_nav ul li').removeClass('link_now');
+		$('.main_nav ul li').eq(secNum).addClass('link_now');
+	}
+};
+
+// スクロール時の表示
+function scrollNav() {
+	scrollPosition = $(window).scrollTop();
+	for (var i = boxTop.length - 1; i >= 0; i--) {
+		if ($(window).scrollTop() > boxTop[i] - set) {
+			changeBox(i);
+			break;
+		}
+	}
+}
+
+ctsHt();
+
+// スクロール時の表示
+$(window).scroll(function() {
+	scrollNav();
+});
+
+changeBox(0);
+
+
 // skill more...
 $('.js_cts_more').click(function(){
 	$(this).closest('.skill_set_cts').addClass('show');
+	setTimeout(function(){
+		ctsHt();
+		scrollNav();
+    },1500);
 });
 
 // coreer more...
 $('.js_career_more').click(function(){
 	$(this).closest('.career_detail').addClass('show');
+	setTimeout(function(){
+		ctsHt();
+		scrollNav();
+    },2000);
 });
 
 // scroll
@@ -18,62 +98,6 @@ $(window).on('load scroll', function(){
 	} else {
 		$('.main_nav').fadeOut(100);
 	}
-});
-
-
-// スムーススクロール
-$(function () {
-	$('a[href^="#"]').click(function () {
-		var adjust = 0;
-		var speed = 500;
-		var href = $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
-		var position = target.offset().top + adjust;
-		$("html, body").animate({ scrollTop: position }, speed, "swing");
-		return false;
-	});
-});
-
-// 現在位置表示
-$(function (){
-	var set = 200 //変化位置
-	var boxTop = [];
-	var current = -1;
-
-	$('.main_cts').each(function(i) {
-		boxTop[i] = $(this).offset().top;
-	});
-
-	// $(window).resize(function(){
-	// 	$('.main_cts').each(function(i) {
-	// 		boxTop[i] = $(this).offset().top;
-	// 	});
-	// 	console.log('resize');
-	// });
-
-	changeBox(0);
-
-	// スクロール時の表示
-	$(window).scroll(function() {
-		scrollPosition = $(window).scrollTop();
-		for (var i = boxTop.length - 1; i >= 0; i--) {
-			if ($(window).scrollTop() > boxTop[i] - set) {
-				changeBox(i);
-				break;
-			}
-		}
-	});
-
-	// navの表示
-	function changeBox(secNum) {
-		if (secNum != current) {
-			current = secNum;
-			// secNum2 = secNum + 1;
-			$('.main_nav ul li').removeClass('link_now');
-			$('.main_nav ul li').eq(secNum).addClass('link_now');
-			
-		}
-	};
 });
 
 
