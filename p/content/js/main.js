435e698fd7a4b7fa0e5fe239d940d48d520860ab
H.yamamoto <h.yamamoto.324@gmail.com>
2020-11-05 11:29:31 +0900

    css

--- p/content/js/main.js
+++ p/content/js/main.js
@@ -1,84 +1,102 @@
-let textHover = [];
-textHover = document.querySelectorAll('.js_hover');
-let body = document.getElementsByTagName('body');
+$(function(){
 
-for( let i = 0;  i < textHover.length; i++) {
-    // hover
-    let d = textHover[i].getAttribute('data-num');
-    let target_l = '_c'+d;
-    let target_d = 'd'+d;
-    textHover[i].addEventListener('mouseenter', () => {
-        textHover[i].classList.add('hover');
-        // 連動先
-        textHover[i].parentNode.classList.add('hover');
-        document.getElementById(target_l).classList.add('hover');
-    }, false);
-    textHover[i].addEventListener('mouseleave', () => {
-        textHover[i].classList.remove('hover');
-        // 連動先
-        textHover[i].parentNode.classList.remove('hover');
-        document.getElementById(target_l).classList.remove('hover');
-    }, false);
-
-    // モーダル
-    textHover[i].onclick = function () {
-        document.getElementById(target_d).classList.add('show');
-        body[0].setAttribute('class','overlay');
-        document.getElementById(target_l).classList.add('now');
-    };
+// loading
+$(window).load(function () {
+	$('.js_load').delay(900).fadeOut(800);
+	$('.js_load_cont').delay(600).fadeOut(300);
+  })
+//10秒たったら強制的にロード画面を非表示
+$(function(){
+	setTimeout(stopload(),10000);
+});
+	
+function stopload(){
+	$('.js_load').delay(900).fadeOut(800);
+	$('.js_load_cont').delay(600).fadeOut(300);
 }
 
-// 逆向きhover
-let lineHover = [];
-lineHover = document.querySelectorAll('.js_hover_line');
+// スムーススクロール
+$('a[href^="#"]').click(function () {
+	var adjust = 0;
+	var speed = 500;
+	var href = $(this).attr("href");
+	var target = $(href == "#" || href == "" ? 'html' : href);
+	var position = target.offset().top + adjust;
+	$("html, body").animate({ scrollTop: position }, speed, "swing");
+	return false;
+});
 
-for( let i = 0;  i < lineHover.length; i++) {
-    let ld = lineHover[i].getAttribute('data-name');
-    let target_ld = ld.replace('c','');
-    lineHover[i].addEventListener('mouseenter', () => {
-        lineHover[i].classList.add('hover');
-        // 連動先
-        document.getElementById(target_ld).classList.add('hover');
-        document.getElementById(target_ld).parentNode.classList.add('hover');
-    }, false);
-    lineHover[i].addEventListener('mouseleave', () => {
-        lineHover[i].classList.remove('hover');
-        // 連動先
-        document.getElementById(target_ld).classList.remove('hover');
-        document.getElementById(target_ld).parentNode.classList.remove('hover');
-    }, false);
+
+// 現在位置表示
+var set = 200 //変化位置
+var boxTop = [];
+var current = -1;
+
+// コンテンツ測定
+function ctsHt() {
+	$('.main_cts').each(function(i) {
+		boxTop[i] = $(this).offset().top;
+	});
 }
 
-// モーダルクローズ
-let modalClose = [];
-modalClose = document.getElementsByClassName('close');
-for (i = 0; i < modalClose.length; i++ ) {
-    let modal_c = "dd_0"+(i+1);
-    let moal_l = "_cd_0"+(i+1);
-    modalClose[i].onclick = function(){
-        body[0].removeAttribute('class');
-        document.getElementById(modal_c).classList.remove('show');
-        document.getElementById(moal_l).classList.remove('now');
-    };
-}
-
-// タブ
-function tabOpen(tab) {
-    let t = tab;
-    let p = [];
-    p = document.querySelectorAll('.js_tab');
-    for(i=0; i<p.length; i++) {
-        if (p[i].classList.contains('page_show')){
-            p[i].classList.remove('page_show');
-        }
-    }
-    document.getElementById(t).classList.add('page_show');
-}
-
-// 初期画面遷移
-let pageDown = document.getElementById('open_btn');
-let mainVisual = document.getElementsByClassName('main_visual');
-pageDown.onclick = function(){
-    mainVisual[0].classList.add('pageDown');
-    mainVisual[0].parentNode.classList.add('bg_white'); 
+// navの表示
+function changeBox(secNum) {
+	if (secNum != current) {
+		current = secNum;
+		// secNum2 = secNum + 1;
+		$('.main_nav ul li').removeClass('link_now');
+		$('.main_nav ul li').eq(secNum).addClass('link_now');
+	}
 };
+
+// スクロール時の表示
+function scrollNav() {
+	scrollPosition = $(window).scrollTop();
+	for (var i = boxTop.length - 1; i >= 0; i--) {
+		if ($(window).scrollTop() > boxTop[i] - set) {
+			changeBox(i);
+			break;
+		}
+	}
+}
+
+ctsHt();
+
+// スクロール時の表示
+$(window).scroll(function() {
+	scrollNav();
+});
+
+changeBox(0);
+
+
+// skill more...
+$('.js_cts_more').click(function(){
+	$(this).closest('.skill_set_cts').addClass('show');
+	setTimeout(function(){
+		ctsHt();
+		scrollNav();
+    },1500);
+});
+
+// coreer more...
+$('.js_career_more').click(function(){
+	$(this).closest('.career_detail').addClass('show');
+	setTimeout(function(){
+		ctsHt();
+		scrollNav();
+    },2000);
+});
+
+// scroll
+var h = $('header').height();
+$(window).on('load scroll', function(){
+	if ($(window).scrollTop() >= h) {
+		$('.main_nav').fadeIn(100);
+	} else {
+		$('.main_nav').fadeOut(100);
+	}
+});
+
+
+});
